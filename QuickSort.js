88aa8a79027dc6c87d154df0d3abb8574e4af926
mild-guy <106711129@gms.tcu.edu.tw>
2020-03-29 20:47:10 +0800

    quick sort implement

---
+++ QuickSort.js
@@ -0,0 +1,20 @@
+const SIZE = 6;
+let list = [7,6,5,4,3,2,1];
+
+function QUICK_SORT(list, left, right){
+	let base=left, i=left, j=right+1;
+	if(i<j){
+		do{
+			do i++; while(list[i]<list[base]&&i<j-1);
+			do j--; while(list[j]>list[base]&&i<=j);
+			if(i<j) list[i]=list[j]+list[i]-(list[j]=list[i]);
+		}while(i<j);
+		list[base]=list[j]+list[base]-(list[j]=list[base]);
+		QUICK_SORT(list, left, j-1);
+		QUICK_SORT(list, j+1, right);
+	}
+}
+
+console.log(list);
+QUICK_SORT(list,0,SIZE);
+console.log(list);
