96035c385d5e988979c62e0e803d0c948eaab52d
Jonah <47838117+JonahYeoh@users.noreply.github.com>
2020-07-11 12:35:44 +0800

    Update QuickSort.js

--- QuickSort.js
+++ QuickSort.js
@@ -2,7 +2,7 @@ const SIZE = 6;
 let list = [7,6,5,4,3,2,1];
 
 function QUICK_SORT(list, left, right){
-	let base=left, i=left, j=right+1;
+	let base=left, i=left, j=right;
 	if(i<j){
 		do{
 			do i++; while(list[i]<list[base]&&i<j-1);
@@ -10,11 +10,11 @@ function QUICK_SORT(list, left, right){
 			if(i<j) list[i]=list[j]+list[i]-(list[j]=list[i]);
 		}while(i<j);
 		list[base]=list[j]+list[base]-(list[j]=list[base]);
-		QUICK_SORT(list, left, j-1);
+		QUICK_SORT(list, left, j);
 		QUICK_SORT(list, j+1, right);
 	}
 }
 
 console.log(list);
-QUICK_SORT(list,0,SIZE);
+QUICK_SORT(list,0,list.length);
 console.log(list);
